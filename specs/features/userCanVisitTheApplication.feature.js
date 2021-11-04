0a7bf3e3ee31a752ed5233084ead26ecac95df2a
giacoletti <giacoletti92@gmail.com>
2021-11-04 21:27:30 +0100

    Adding end2end test for visibility of elements on index.html adding basic elements on index.html to add a new contact

--- specs/features/userCanVisitTheApplication.feature.js
+++ specs/features/userCanVisitTheApplication.feature.js
@@ -1,8 +1,24 @@
 describe('User visiting the application url', () => {
-
-    it('is expected to see a header', () => {
+    
+    before(() => {
         cy.visit('/');
+    });
+
+    it('is expected to see the index page', () => {
         cy.get('h1').should('contain.text', 'Address Book');
+        cy.get('button[id=add-contact]').should('be.visible').should('contain.text', 'Add New Contact');
+        cy.get('#nameLabel').should('contain.text', 'Name');
+        cy.get('#name').should('exist');
+        cy.get('#emailLabel').should('contain.text', 'Email');
+        cy.get('#email').should('exist');
+        cy.get('#phoneLabel').should('contain.text', 'Phone');
+        cy.get('#phone').should('exist');
+        cy.get('#companyLabel').should('contain.text', 'Company');
+        cy.get('#company').should('exist');
+        cy.get('#notesLabel').should('contain.text', 'Notes');
+        cy.get('#notes').should('exist');
+        cy.get('#twitterLabel').should('contain.text', 'Twitter');
+        cy.get('#twitter').should('exist');
     });
     
 });
