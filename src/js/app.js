20bb035c1a32f8d9b918e16beee2b371b7c76264
giacoletti <giacoletti92@gmail.com>
2022-01-07 22:44:25 +0100

    Installs browser-sync creates serve.js for browser-sync configuration adds start:hot script to package.json adds message if there are no contacts in memory

--- src/js/app.js
+++ src/js/app.js
@@ -1,34 +1,41 @@
 const addressBook = new AddressBook();
-const addressList = document.querySelector("[name=address_list] ul");
-const messageElement = document.getElementById("response_message");
+const addressList = document.querySelector('[name=address_list] ul');
+const messageElement = document.getElementById('response_message');
 
 const displayEntries = () => {
-    addressList.innerHTML = "";
-    const entries = addressBook.index();
-    let html;
+  addressList.innerHTML = '';
+  const entries = addressBook.index();
+  let html;
+  if (entries) {
     entries.forEach((entry) => {
-        html = `Name: ${entry.name} `;
-        html += `Phone: ${entry.phone} `;
-        html += `Twitter: ${entry.twitter}`;
-        let newListItem = document.createElement("li");
-        newListItem.innerHTML = html;
-        addressList.appendChild(newListItem);
+      html = `Name: ${entry.name} `;
+      html += `Phone: ${entry.phone} `;
+      html += `Twitter: ${entry.twitter}`;
+      let newListItem = document.createElement('li');
+      newListItem.innerHTML = html;
+      addressList.appendChild(newListItem);
     });
+  } else {
+    html = 'You have no contacts in your address book.';
+    let newParagraph = document.createElement('p');
+    newParagraph.innerHTML = html;
+    addressList.appendChild(newParagraph);
+  }
 };
 
 const handleForm = (form) => {
-    if (form.elements.name.value.trim() === "") {
-        messageElement.innerText = "Please enter a name!";
-    } else {
-        const name = form.elements.name.value;
-        const phone = form.elements.phone.value;
-        const twitter = form.elements.twitter.value;
-        addressBook.create({ name: name, phone: phone, twitter: twitter });
-        form.reset();
-        displayEntries();
-        messageElement.innerText = "Contact saved!";
-    }
-    return false;   //it prevents the page from reloading
+  if (form.elements.name.value.trim() === '') {
+    messageElement.innerText = 'Please enter a name!';
+  } else {
+    const name = form.elements.name.value;
+    const phone = form.elements.phone.value;
+    const twitter = form.elements.twitter.value;
+    addressBook.create({ name: name, phone: phone, twitter: twitter });
+    form.reset();
+    displayEntries();
+    messageElement.innerText = 'Contact saved!';
+  }
+  return false; //it prevents the page from reloading
 };
 
 displayEntries();
